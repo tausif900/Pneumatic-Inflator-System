393ffa45980de7e7e4b9595a35860ce6ac69fc9c
tausif900 <ansaritausif9268@gmail.com>
2026-02-21 08:24:55 +0530

    login

--- Pneumatic Inflator System/src/components/Login.jsx
+++ Pneumatic Inflator System/src/components/Login.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useForm } from "react-hook-form";
 
 function Login() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const submitHandler = (data) => {
+    console.log(data);
+
+  }
   return (
     <div>
-      Login
+      {/* <!-- login page --> */}
+      <div className="container d-flex justify-content-center align-items-center vh-100 ">
+        <div className="card p-4  p-3 mb-5 bg-white rounded cust-login">
+          <h3 className="text-center mb-4">Login</h3>
+          <form onSubmit={handleSubmit(submitHandler)}>
+            <div className="mb-3">
+              <label htmlFor="emailInput" className="form-label">EMAIL</label>
+              <input type="email" className="form-control input-login" id="emailInput" aria-describedby="emailHelp"
+                placeholder="Enter username"{...register('email', { required: 'email is required', minLength: { value: 10, message: 'email must contain atleast 10 character long' } })} />
+                {errors.email && <p className='error-msg'>{errors.email.message}</p>}
+            </div>
+            <div className="mb-3">
+              <label htmlFor="passwordInput" className="form-label">PASSWORD</label>
+              <input type="password" className="form-control
+                        input-login"
+                placeholder='Enter Password'
+                id="passwordInput" />
+            </div>
+            <button type="login" className="kg-cust-login-btn w-25">Login</button>
+            <div className="form-text text-start">Not a user?
+              <Link className="text-decoration-underline" to={'registration'}>Register
+              </Link>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
--- Pneumatic Inflator System/src/components/Registration.jsx
+++ Pneumatic Inflator System/src/components/Registration.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Registration() {
   return (
     <div>
-      Registration
+       {/* <!-- Registration page --> */}
+        <div class="container d-flex justify-content-center align-items-center vh-100">
+            <div class="card p-4  p-3 mb-5 bg-white rounded cust-register">
+                <h3 class="text-center mb-4">Registration</h3>
+                <form>
+                    <div class="mb-3">
+                        <label for="userInput" class="form-label">USERNAME:</label>
+                        <input type="username" class="form-control input-login" id="userInput" placeholder="Enter username"/>
+                    </div>
+                    <div class="mb-3">
+                        <label for="emailInput" class="form-label">EMAIL ID:</label>
+                        <input type="email" class="form-control input-login" id="emailInput" aria-describedby="emailHelp"
+                            placeholder="Enter a valid email address"/>
+                    </div>
+                    <div class="mb-3">
+                        <label for="passwordInput" class="form-label">PASSWORD</label>
+                        <input type="password" class="form-control input-login" id="passwordInput"
+                        placeholder='password'/>
+                    </div>
+                    <div class="mb-3">
+                        <label for="passwordInput" class="form-label ">CONFIRM PASSWORD</label>
+                        <input type="password" class="form-control  input-login " id="passwordInput"
+                        placeholder='Confirm Password'/>
+                    </div>
+                    <button type="login" class="btn btn-primary w-25 kg-cust-Register-btn">Register</button>
+                    <div class="form-text text-start">Already User?
+                        <Link class="text-decoration-underline" to={'login'}>Login
+                        </Link>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
   )
 }
