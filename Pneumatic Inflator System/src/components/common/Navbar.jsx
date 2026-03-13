e10c37624a705bf25f9bcd439dccfdd1abfa99e6
tausif900 <ansaritausif9268@gmail.com>
2026-03-13 15:49:16 +0530

    navbar

--- Pneumatic Inflator System/src/components/common/Navbar.jsx
+++ Pneumatic Inflator System/src/components/common/Navbar.jsx
@@ -7,14 +7,11 @@ function Navbar() {
         <div>
             <nav className="navbar navbar-expand-lg nav-clr">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
-                        <img src={logo} alt="PneumaticInflatorSytem" className="d-inline-block align-text-top kg-img " />
-                    </a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mx-auto gap-5">
+                    <div className="collapse navbar-collapse d-flex justify-content-evenly list-unstyled" id="navbarSupportedContent">
+                        <ul className="navbar-nav">
                             <li className="nav-item">
                                 <Link className="nav-link active fw-bold fs-5" aria-current="page" to={'/'}>Home</Link>
                             </li>
