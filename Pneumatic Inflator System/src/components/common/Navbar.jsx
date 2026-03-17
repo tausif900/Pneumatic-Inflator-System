ea4a123f88e4dbe7693e1cfbedf41512bd323f4d
tausif900 <ansaritausif9268@gmail.com>
2026-03-17 20:16:44 +0530

    css

--- Pneumatic Inflator System/src/components/common/Navbar.jsx
+++ Pneumatic Inflator System/src/components/common/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4 cust-nav">
+      <nav className={`navbar navbar-expand-lg navbar-dark bg-dark px-4 ${styles.navbarCustom}`}>
         <Link className="navbar-brand" to="/">
           Pneumatic Inflator System
         </Link>
@@ -22,15 +23,15 @@ const Navbar = () => {
           <ul className="navbar-nav me-auto">
 
             <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
+              <Link className={`nav-link ${styles.link}`} to="/">Home</Link>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
+              <Link className={`nav-link ${styles.link}`} to="/about">About</Link>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link" to="/contact">Contact</Link>
+              <Link className={`nav-link ${styles.link}`} to="/contact">Contact</Link>
             </li>
 
           </ul>
@@ -38,11 +39,11 @@ const Navbar = () => {
           <ul className="navbar-nav">
 
             <li className="nav-item">
-              <Link className="nav-link" to="/login">Login</Link>
+              <Link className={`nav-link ${styles.link}`} to="/login">Login</Link>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link" to="/register">Register</Link>
+              <Link className={`nav-link ${styles.link}`} to="/register">Register</Link>
             </li>
 
           </ul>
